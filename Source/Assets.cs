d61d8857ae01e7d1397af4695aeac03f8870ae95
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-06-04 18:21:18 -0700

    try to fix inconsistent issue with picking revolver bullet asset

--- Source/Assets.cs
+++ Source/Assets.cs
@@ -7,30 +7,52 @@ namespace Nyxpiri.ULTRAKILL.FeedbackersForEveryone
     public static class Assets
     {
         public static AssetReference ParryFlashPrefab { get; private set; } = null;
-        
+
         public static GameObject EnemyRevolverBullet { get; private set; } = null;
         public static GameObject EnemyRevolverAltBullet { get; private set; } = null;
+
+        private static GameObject _prefabHolder = null;
+        public static GameObject PrefabHolder
+        {
+            get
+            {
+                if (_prefabHolder == null)
+                {
+                    _prefabHolder = new GameObject();
+                    GameObject.DontDestroyOnLoad(_prefabHolder);
+                    _prefabHolder.SetActive(false);
+                }
+
+                return _prefabHolder;
+            }
+        }
 
         internal static void Initialize()
         {
             NyxLib.Assets.AddAssetPicker<SwordsMachine>((sm) =>
             {
                 ParryFlashPrefab = sm.gunFlash;
-                
-                return true; 
+
+                return true;
             });
 
             NyxLib.Assets.AddAssetPicker<EnemyRevolver>((revolver) =>
             {
-                EnemyRevolverBullet = GameObject.Instantiate(revolver.bullet);
+                if (revolver.bullet.GetComponent<Projectile>() == null)
+                {
+                    return false;
+                }
+
+
+                EnemyRevolverBullet = GameObject.Instantiate(revolver.bullet, PrefabHolder.transform);
                 EnemyRevolverBullet.SetActive(false);
                 UnityEngine.Object.DontDestroyOnLoad(EnemyRevolverBullet);
-            
-                EnemyRevolverAltBullet = GameObject.Instantiate(revolver.altBullet);
+
+                EnemyRevolverAltBullet = GameObject.Instantiate(revolver.altBullet, PrefabHolder.transform);
                 EnemyRevolverAltBullet.SetActive(false);
                 UnityEngine.Object.DontDestroyOnLoad(EnemyRevolverAltBullet);
-            
-                return true; 
+
+                return true;
             });
         }
     }
